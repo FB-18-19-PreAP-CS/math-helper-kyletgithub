99ded24ba6e44d1b6543e2c960053b5c15d721ef
THREATT.KYLE <2200796@fbisd.flourbluffschools.net>
2018-09-18 14:12:04 -0500

    quadratic formula

--- math_helper-threattk.py
+++ math_helper-threattk.py
@@ -16,6 +16,36 @@ def distance(x1,y1,x2,y2):
     d = round(d, 2)
     return d
 
+def pythag(a, b):
+    ''' returns the length of the hypotenuse
+        in a right triangle
+
+        >>> pythag(0,0)
+        0.0
+        
+        
+    '''
+    c = sqrt(a**2 + b**2)
+    c = round(c, 2)
+    return c
+
+def quadratic_form(a, b, c):
+    ''' applies the quadratic formula to a, b and c
+        and returns the zeros
+        
+        >>> quadratic_form(1, 5, 10)
+        "function doesn't cross x-axis"
+        
+    '''
+    d = b**2 - 4 * a * c
+    if d < 0:
+        return "function doesn't cross x-axis"
+    zero1 = ((-b + sqrt(d))/2*a)
+    zero2 = ((-b - sqrt(d))/2*a)
+    zero1 = round(zero1, 1)
+    zero2 = round(zero2, 1)
+    
+    return (zero1, 0), (zero2, 0)
 
 if __name__ == "__main__":
     import doctest
